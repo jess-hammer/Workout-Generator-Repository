d7d68fefcb0ea2bb15df359fc2c815bc7613dccc
jess-hammer <59108399+jess-hammer@users.noreply.github.com>
2020-09-13 17:55:05 +1000

    implemented basic timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Attach me to the textbox you want to display the timer!
+public class Timer : MonoBehaviour
+{
+	private static float timeLeft = 5; // in seconds
+	public static Text textBox; 
+
+	public void setTimer (int minutes,  int seconds) {
+		timeLeft = (minutes * 60) + seconds;
+	}
+
+	private void Start () {
+		textBox = this.GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		textBox.text = ((int)timeLeft / 60) + ":" + ((int)timeLeft % 60);
+
+		if ((int)timeLeft > 0) {
+			timeLeft -= Time.deltaTime;
+		} 
+		
+    }
+}
