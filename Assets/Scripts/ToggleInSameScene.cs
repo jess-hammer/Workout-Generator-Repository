a0bf67285abaa606ea58a35eafad209d7e05cd2b
jess-hammer <59108399+jess-hammer@users.noreply.github.com>
2021-02-20 19:59:14 +1000

    Added select body focus icons and functionality

--- Assets/Scripts/ToggleInSameScene.cs
+++ Assets/Scripts/ToggleInSameScene.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class ToggleInSameScene : MonoBehaviour
 {
 	public GameObject [] customiserObjects;
+	public GameObject [] customiser2Objects;
 	public GameObject [] overviewObjects;
 	public UserPrefs userPrefs;
 
 
-    public void CustomiserToOverview()
+    public void Page1To2()
 	{
 		if (userPrefs.isValid ()) {
-			setArrayActive (overviewObjects, true);
+			setArrayActive (customiser2Objects, true);
 			setArrayActive (customiserObjects, false);
 		}
 	}
 
-	public void OverviewToCustomiser ()
+	public void Page2To1 ()
 	{
-		setArrayActive (customiserObjects, true);
-		setArrayActive (overviewObjects, false);
+		if (userPrefs.isValid ()) {
+			setArrayActive (customiserObjects, true);
+			setArrayActive (customiser2Objects, false);
+		}
 	}
 
+	public void Page2To3 ()
+	{
+		if (userPrefs.isValid ()) {
+			setArrayActive (overviewObjects, true);
+			setArrayActive (customiser2Objects, false);
+		}
+	}
+
+	public void Page3To2 ()
+	{
+		if (userPrefs.isValid ()) {
+			setArrayActive (customiser2Objects, true);
+			setArrayActive (overviewObjects, false);
+		}
+	}
+
+	
 	private void setArrayActive(GameObject [] array, bool active)
 	{
 		for (int i = 0; i < array.Length; i++) {
