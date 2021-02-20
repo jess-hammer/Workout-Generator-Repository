fa6bdc53bca524501b84531d27f6ebfea54bdcc2
jess-hammer <59108399+jess-hammer@users.noreply.github.com>
2021-02-20 20:47:47 +1000

    Changed to tmpro in main screen

---
+++ Assets/DisplayOverviewTextmesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayOverviewTextmesh : MonoBehaviour
+{
+	public Generator generator;
+	public UserPrefs userPrefs;
+	public TextMeshProUGUI duration;
+	public TextMeshProUGUI difficulty;
+	public TextMeshProUGUI workout;
+
+	// Start is called before the first frame update
+	void OnEnable ()
+	{
+		generator.CreateWorkout ();
+		difficulty.text = "Difficulty: " + userPrefs.difficultyToString ();
+		duration.text = "Duration: " + userPrefs.totalDuration + " min";
+		workout.text = generator.toString ();
+	}
+}
--- Assets/Scripts/ImplementWorkout.cs
+++ Assets/Scripts/ImplementWorkout.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,8 +9,8 @@ public class ImplementWorkout : MonoBehaviour
 	public bool isRest = false;
 	private float totalTimeLeft;
 	private float totalDuration;
-	public Text textbox;
-	public Text nextTextbox;
+	public TextMeshProUGUI textbox;
+	public TextMeshProUGUI nextTextbox;
 	public Timer timer;
 	public Image exerciseIcon;
 	private Animator anim;
