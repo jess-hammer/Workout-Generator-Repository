4be6e034208dd5fb5b07d92ce95d5400ed6360c9
jess-hammer <59108399+jess-hammer@users.noreply.github.com>
2021-02-23 16:22:50 +1000

    Added copy to clipboard and refresh button

--- Assets/DisplayOverviewTextmesh.cs
+++ Assets/DisplayOverviewTextmesh.cs
@@ -25,4 +25,27 @@ public class DisplayOverviewTextmesh : MonoBehaviour
 		type.text = "Workout Type: " + userPrefs.ListToString<Type>(userPrefs.workoutType);
 		workout.text = generator.toString ();
 	}
+
+	public void CopyToClipboard()
+	{
+		string str = "";
+		str += difficulty.text + "\n";
+		str += duration.text + "\n";
+		str += focus.text + "\n";
+		str += equipment.text + "\n";
+		str += type.text + "\n";
+		str += workout.text + "\n";
+		ClipboardExtension.CopyToClipboard (str);
+	}
+
+	public void Refresh()
+	{
+		generator.CreateWorkout ();
+		difficulty.text = "Difficulty: " + userPrefs.difficultyToString ();
+		duration.text = "Duration: " + userPrefs.totalDuration + " min";
+		focus.text = "Focus: " + userPrefs.focusAreaToString ();
+		equipment.text = "Equipment: " + userPrefs.ListToString<Equipment> (userPrefs.equipment);
+		type.text = "Workout Type: " + userPrefs.ListToString<Type> (userPrefs.workoutType);
+		workout.text = generator.toString ();
+	}
 }
